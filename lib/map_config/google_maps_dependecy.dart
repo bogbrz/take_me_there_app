302d3cc08f011c40cca8c242220964a42128fb37
BogBrz <142913714+bogbrz@users.noreply.github.com>
2024-05-25 12:22:31 +0200

    Update google_maps_dependecy.dart

--- lib/map_config/google_maps_dependecy.dart
+++ lib/map_config/google_maps_dependecy.dart
@@ -1,6 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-String googleMapKey = "AIzaSyCJtDbYQPyMOXIdSD_jm7NQHRb2pI3tyEA";
+
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
   target: LatLng(37.1, -122.085749655962),
